365eff626ca122d8029cbdfe1dc42cdaa6f7c1cb
GutemaG <bir13gud17@gmail.com>
2022-12-22 23:22:16 +0300

    --amend

--- src/modules/refreshPage.js
+++ src/modules/refreshPage.js
@@ -2,7 +2,7 @@ import getData from './getData.js';
 import displayScore from './displayScore.js';
 
 const refreshPage = () => {
-  const ID = 'notionAPI123';
+  const ID = 'birhanu123';
   const result = getData(ID);
   result.then((resp) => displayScore(resp));
 };
